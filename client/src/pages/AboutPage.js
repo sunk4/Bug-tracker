a417570a3179114fa3295d9a62b8251530f08cf2
Roman Trnka <trnka.roman13@gmail.com>
2022-06-30 14:47:55 +0200

    about page fixed and finished

--- client/src/pages/AboutPage.js
+++ client/src/pages/AboutPage.js
@@ -6,45 +6,39 @@ import godImg from '../assets/images/god.jpg'
 
 const AboutPage = () => {
   return (
-    <main className="full-page">
+    <Wrapper>
       <Navbar />
-      <Wrapper>
-        <section className="about-section">
-          <section className="first-member member">
-            <img src={googleImg} alt="google" />
-            <h4>Google</h4>
-            <p>
-              Minim labore duis labore id. Labore labore aliquip velit nostrud
-              incididunt quis labore adipisicing. Non eiusmod dolor ipsum cillum
-              ipsum sit nostrud esse nisi cillum sunt proident elit. Sint irure
-              Lorem laborum duis fugiat fugiat cillum est eiusmod exercitation
-              fugiat.
-            </p>
-          </section>
-          <section className="second-member member">
-            <img src={meImg} alt="me" />
-            <h4>Roman Trnka</h4>
-            <p>
-              My name is Roman Trnka and this is my first fullstack App - using
-              MERN STACK.
-            </p>
-          </section>
-          <section className="third-member member">
-            <img src={godImg} alt="god" />
-            <h4>God</h4>
-            <p>
-              Minim labore duis labore id. Labore labore aliquip velit nostrud
-              incididunt quis labore adipisicing. Non eiusmod dolor ipsum cillum
-              ipsum sit nostrud esse nisi cillum sunt proident elit. Sint irure
-              Lorem laborum duis fugiat fugiat cillum est eiusmod exercitation
-              fugiat.
-            </p>
-          </section>
+      <main className="about-main">
+        <section className="team-member-section">
+          <img className="team-member-img" src={googleImg} alt="google" />
+          <h4>Google</h4>
+          <p>
+            Google is my best friend since 2008. He helps me with everything. He
+            even finishes my sentences. And sometimes I say something stupid,
+            and he is like, did you mean ..... And I am like, that's exactly
+            what I meant. I love google, we are inseparable
+          </p>
         </section>
-      </Wrapper>
-
+        <section className="team-member-section">
+          <img className="team-member-img" src={meImg} alt="me" />
+          <h4>Roman Trnka</h4>
+          <p>
+            My name is Roman Trnka. I'm a passionate child who lives in
+            Bratislava and passionate software engineer who will do whatever it
+            takes to find and answer.
+          </p>
+        </section>
+        <section className="team-member-section">
+          <img className="team-member-img" src={godImg} alt="god" />
+          <h4>God</h4>
+          <p>
+            When I wrote this code, only God and I understood what it did.
+            Now... Only god knows.
+          </p>
+        </section>
+      </main>
       <Footer />
-    </main>
+    </Wrapper>
   )
 }
 
