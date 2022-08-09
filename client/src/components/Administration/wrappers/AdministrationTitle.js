7d734a88478740aeee20824d580af1d4afaf5d22
Roman Trnka <trnka.roman13@gmail.com>
2022-08-09 07:52:39 +0200

    styling

--- client/src/components/Administration/wrappers/AdministrationTitle.js
+++ client/src/components/Administration/wrappers/AdministrationTitle.js
@@ -1,5 +1,21 @@
 import styled from 'styled-components'
 
-const Wrapper = styled.div``
+const Wrapper = styled.div`
+  .main-title {
+    background-color: var(--grey-200);
+  }
+  .title-administration {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: 0 auto;
+  }
+
+  h5 {
+    text-decoration: underline;
+
+    text-align: start;
+  }
+`
 
 export default Wrapper
