30c3976f1905bcf28bfe3e0a2352981acb7aa0f8
Roman Trnka <trnka.roman13@gmail.com>
2022-08-15 13:16:43 +0200

    styling SharedLayout

--- client/src/pages/dashboard/SharedLayout.js
+++ client/src/pages/dashboard/SharedLayout.js
@@ -1,11 +1,10 @@
 import { Outlet } from 'react-router-dom'
-import { Header, Sidebar } from '../../components/SharedLayout'
+import { Sidebar } from '../../components/SharedLayout'
 import Wrapper from '../../assets/wrappers/SharedLayout'
 
 const SharedLayout = () => {
   return (
     <Wrapper>
-      <Header />
       <Sidebar />
       <Outlet />
     </Wrapper>
