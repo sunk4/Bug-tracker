57e297f06e7037dbf569a64b145b3c9011dac936
Roman Trnka <trnka.roman13@gmail.com>
2022-08-16 11:23:45 +0200

    changed components for SingleTicket

--- client/src/components/SingleProject/TicketsContainer.js
+++ client/src/components/SingleProject/TicketsContainer.js
@@ -5,11 +5,27 @@ import { useTicketsContext } from '../../context/ticketsContext'
 import { AiFillDelete } from 'react-icons/ai'
 import { FiEdit } from 'react-icons/fi'
 import Wrapper from './wrappers/TicketsContainer'
+import { useState } from 'react'
+import { SingleTicketInfo } from '../../components/SingleProject'
+import {
+  CreateTicketModal,
+  UpdateTicketModal,
+} from '../../components/SingleProject'
 
-const TicketsContainerInProject = ({ ticketsAll }) => {
+const TicketsContainerInProject = ({
+  ticketsAll,
+  singleTicket,
+
+  showModal,
+  dataModal,
+  projectName,
+  ticketId,
+}) => {
   const { displayModal } = useAppContext()
   const { getSingleUser, singleUser } = useUsersContext()
   const { deleteTicket, getSingleTicket } = useTicketsContext()
+  const [showSingleTicket, setShowSingleTicket] = useState(false)
+  console.log()
 
   // useEffect(() => {
   //   getSingleUser(userId)
@@ -19,36 +35,61 @@ const TicketsContainerInProject = ({ ticketsAll }) => {
   return (
     <Wrapper>
       <div>
+        <h4>Tickets</h4>
+        <button
+          className="btn"
+          data-modal="modal-create-ticket"
+          onClick={displayModal}
+          type="button"
+        >
+          New Ticket
+        </button>
+
+        {showModal && dataModal === 'modal-create-ticket' && (
+          <CreateTicketModal projectName={projectName} />
+        )}
+
+        {showModal && dataModal === 'modal-edit-project' && (
+          <UpdateTicketModal projectName={projectName} ticketId={ticketId} />
+        )}
+
         <h5>Title</h5>
         <h5>Description</h5>
         <h5>Name</h5>
+        <h5>Actions</h5>
       </div>
       {ticketsAll.map((ticket) => {
         const {
           ticketTitle,
           ticketDescription,
-          ticketId,
+          _id: ticketId,
           firstName,
           lastName,
         } = ticket
         return (
-          <div key={ticketId}>
-            <div onClick={() => getSingleTicket(ticketId)}>
+          <div key={ticketId} onClick={() => getSingleTicket(ticketId)}>
+            <button type="button" onClick={() => setShowSingleTicket(true)}>
               <h5>{ticketTitle}</h5>
               <h5>{ticketDescription}</h5>
               <h5>
                 {firstName} {lastName}
               </h5>
+            </button>
+            <div>
+              <button type="button">
+                <AiFillDelete onClick={() => deleteTicket(ticketId)} />
+              </button>
+              <button type="button">
+                <FiEdit
+                  data-modal="modal-edit-project"
+                  onClick={displayModal}
+                />
+              </button>
             </div>
-            <button type="button">
-              <AiFillDelete onClick={() => deleteTicket(ticketId)} />
-            </button>
-            <button type="button">
-              <FiEdit data-modal="modal-edit-project" onClick={displayModal} />
-            </button>
           </div>
         )
       })}
+      {showSingleTicket && <SingleTicketInfo {...singleTicket} />}
     </Wrapper>
   )
 }
